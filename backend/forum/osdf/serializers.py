dd87d8c763fd23f37780614151b7a36239e092a9
GolamAli1105 <golam.ali1105@gmail.com>
2024-11-13 02:32:11 +0530

    token implementation

---
+++ backend/forum/osdf/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from .models import Post
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id","username","password"]
+        extra_kwargs = {"password": {"write_only": True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
+    
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        field = ["id","title","content","created_at","author","likes"]
+        extra_kwargs = {"author":{"read_only": True}}
+
+    
